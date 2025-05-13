b4ed1ba36ede51e99cfcbafc0871269032ab5fc0
John Imeobong <imeobongjohn38@gmail.com>
2025-05-13 13:44:43 +0100

    Display US_flag

---
+++ US_Flag.java
@@ -0,0 +1,13 @@
+public class US_Flag {
+	public static void main(String[] args) {
+
+for(int count = 1; count <= 8; count++){
+System.out.println("* * * * * * * * * " + " ====================");
+}
+for(int count = 1; count <= 8; count++){
+System.out.println("=======================================");
+}
+
+
+}
+}
